6b99d5c65461e06da87f43db7108d751841d7232
David Chanin <chanindav@gmail.com>
2021-08-09 17:50:05 +0100

    fancier model

--- chess_ai/ChessValueModel.py
+++ chess_ai/ChessValueModel.py
@@ -1,28 +1,39 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class SkipBlock(nn.Module):
+    def __init__(self, hidden_size: int):
+        self.layer = nn.Sequential(
+            [
+                nn.Linear(hidden_size, hidden_size),
+                nn.BatchNorm1d(hidden_size),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.8),
+            ]
+        )
+
+    def forward(self, x):
+        out = self.layer(x)
+        return out + x
+
+
 class ChessValueModel(nn.Module):
-  def __init__(self, hidden_size = 128):
-    super().__init__()
-    self.layer1 = nn.Linear(901, hidden_size)
-    self.bn1 = nn.BatchNorm1d(hidden_size)
-    self.dropout1 = nn.Dropout(0.8)
+    def __init__(self, hidden_size=128, num_blocks=8):
+        super().__init__()
+        self.in_layer = nn.Sequential(
+            [
+                nn.Linear(901, hidden_size),
+                nn.BatchNorm1d(hidden_size),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.8),
+            ]
+        )
+        self.blocks = nn.Sequential([SkipBlock(hidden_size) for _ in range(num_blocks)])
+        self.out_layer = nn.Linear(hidden_size, 1)
 
-    self.layer2 = nn.Linear(hidden_size, hidden_size)
-    self.bn2 = nn.BatchNorm1d(hidden_size)
-    self.dropout2 = nn.Dropout(0.8)
-
-    self.layer3 = nn.Linear(hidden_size, hidden_size)
-    self.bn3 = nn.BatchNorm1d(hidden_size)
-    self.dropout3 = nn.Dropout(0.8)
-
-    self.out_layer = nn.Linear(hidden_size, 1)
-
-  def forward(self, x):
-    x = F.relu(self.dropout1(self.bn1(self.layer1(x))))
-    x = F.relu(self.dropout2(self.bn2(self.layer2(x))))
-    x = F.relu(self.dropout3(self.bn3(self.layer3(x))))
-    x = self.out_layer(x)
-
-    # value output
-    return F.tanh(x)
+    def forward(self, x):
+        x = self.in_layer(x)
+        x = self.blocks(x)
+        x = self.out_layer(x)
+        return F.tanh(x)
