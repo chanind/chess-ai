353612f32cfe8518f1d29c041e31ac798ddabf59
David Chanin <chanindav@gmail.com>
2021-08-13 18:23:15 +0200

    adding coode for turning action -> tensor coords

--- chess_ai/State.py
+++ chess_ai/State.py
@@ -7,26 +7,121 @@ from torch.nn.functional import one_hot
 import numpy as np
 
 
+QUEEN_DIRECTIONAL_MAPPING = {
+    (-1, 0): 0,
+    (-1, -1): 1,
+    (-1, 1): 2,
+    (0, -1): 3,
+    (0, 1): 4,
+    (1, 1): 5,
+    (1, 0): 6,
+    (1, -1): 7,
+}
+
+KNIGHT_DIRECTIONAL_MAPPING = {
+    (1, 2): 0,
+    (1, -2): 1,
+    (2, 1): 2,
+    (2, -1): 3,
+    (-1, 2): 4,
+    (-1, -2): 5,
+    (-2, 1): 6,
+    (-2, -1): 7,
+}
+
+UNDERPROMOTION_TYPE_MAPPING = {
+    chess.KNIGHT: 0,
+    chess.ROOK: 1,
+    chess.BISHOP: 2,
+}
+UNDERPROMOTION_DIRECTIONAL_MAPPING = {
+    0: 0,
+    1: 1,
+    -1: 2,
+}
+
+
 def serialization_to_tensor(
     serialization: Tuple[np.array, np.array, Optional[int], int]
 ) -> torch.Tensor:
-    (pieces_state, castling_state, en_passant_state, turn) = serialization
+    (pieces_state, castling_state, en_passant_state, turn, move_number) = serialization
     one_hot_pieces_state = one_hot(
         torch.from_numpy(pieces_state).type(torch.int64), num_classes=13
-    ).view((-1))
-    one_hot_en_passant_state = torch.zeros(64, dtype=torch.float)
+    ).view((8, 8, -1))
+    one_hot_en_passant_state = torch.zeros((8, 8, 1), dtype=torch.float)
     if en_passant_state is not None:
-        en_passant_state = one_hot(torch.tensor(en_passant_state), num_classes=64)
-    castling_state_tensor = torch.from_numpy(castling_state)
+        one_hot_en_passant_state = one_hot(
+            torch.tensor(en_passant_state), num_classes=64
+        ).view((8, 8, 1))
+    castling_state_tensor = torch.from_numpy(castling_state).expand(8, 8, -1)
     bstate = torch.cat(
         [
             one_hot_pieces_state,
             castling_state_tensor,
             one_hot_en_passant_state,
-            torch.tensor(turn * 1.0).unsqueeze(0),
-        ]
-    )
+            torch.tensor(turn * 1.0).expand(8, 8, 1),
+            torch.tensor(move_number * 1.0).expand(8, 8, 1),
+        ],
+        dim=-1,
+    ).transpose(0, 2)
     return bstate.float()
+
+
+def get_coords(square) -> Tuple[int, int]:
+    x_coord = square % 8
+    y_coord = square // 8
+    return (x_coord, y_coord)
+
+
+def func_compare(val1, val2) -> int:
+    if val1 == val2:
+        return 0
+    return 1 if val1 > val2 else -1
+
+
+def process_move_coords(move) -> Tuple[int, int, int]:
+    """
+    given a move, output the x, y, and move_type for the move
+    This represents the coordinate of the output that will be 1, the rest will be 0
+    """
+    x_coord, y_coord = get_coords(move.from_square)
+    to_coords = get_coords(move.to_square)
+
+    key_x = func_compare(to_coords[0], x_coord)
+    key_y = func_compare(to_coords[1], y_coord)
+
+    delta_x = to_coords[0] - x_coord
+    delta_y = to_coords[1] - y_coord
+
+    is_knight_move = (
+        min(abs(delta_x), abs(delta_y)) == 1 and max(abs(delta_x), abs(delta_y)) == 2
+    )
+    is_underpromotion = move.promotion is not None and move.promotion != chess.QUEEN
+
+    if is_underpromotion:
+        # underpromotion values are 64-72
+        direction_key = key_x
+        underpromotion_value = UNDERPROMOTION_TYPE_MAPPING[move.promotion]
+        underpromotion_direction = UNDERPROMOTION_DIRECTIONAL_MAPPING[direction_key]
+        return (
+            x_coord,
+            y_coord,
+            underpromotion_direction + (underpromotion_value * 3) + 64,
+        )
+
+    elif is_knight_move:
+        # knight moves are 56-63
+        direction_key = (delta_x, delta_y)
+        return (x_coord, y_coord, KNIGHT_DIRECTIONAL_MAPPING[direction_key] + 56)
+    else:
+        # queen moves are 0-55
+        direction_key = (key_x, key_y)
+        magnitude = max(abs(delta_x), abs(delta_y))
+        return (
+            x_coord,
+            y_coord,
+            QUEEN_DIRECTIONAL_MAPPING[direction_key] + (magnitude - 1) * 8,
+        )
 
 
 class State(object):
@@ -47,11 +142,11 @@ class State(object):
     def serialize(self) -> Tuple[np.array, np.array, Optional[int], int]:
         assert self.board.is_valid()
 
-        pieces_state = np.zeros(64, np.uint8)
+        pieces_state_linear = np.zeros(64, np.uint8)
         for i in range(64):
             pp = self.board.piece_at(i)
             if pp is not None:
-                pieces_state[i] = {
+                pieces_state_linear[i] = {
                     "P": 1,
                     "N": 2,
                     "B": 3,
@@ -65,6 +160,7 @@ class State(object):
                     "q": 11,
                     "k": 12,
                 }[pp.symbol()]
+        pieces_state = pieces_state_linear.reshape((8, 8))
 
         castling_state = np.zeros(4, np.uint8)
         if self.board.has_queenside_castling_rights(chess.WHITE):
@@ -79,7 +175,8 @@ class State(object):
         en_passant_state = self.board.ep_square
 
         turn = self.board.turn
+        move_number = min(len(self.board.move_stack) / 250.0, 1)
         # bstate = torch.cat([pieces_state, castling_state, en_passant_state, turn])
 
         # returning these partially processed parts as a tuple to have lower memory usage in dataloader
-        return (pieces_state, castling_state, en_passant_state, turn)
+        return (pieces_state, castling_state, en_passant_state, turn, move_number)
--- tests/test_State.py
+++ tests/test_State.py
@@ -1,6 +1,5 @@
 import chess
-import torch
-from chess_ai.State import State, serialization_to_tensor
+from chess_ai.State import State, serialization_to_tensor, process_move_coords
 
 
 def test_serialize_state_to_tensor_dims():
@@ -8,4 +7,38 @@ def test_serialize_state_to_tensor_dims():
     state = State(board)
     serialization = state.serialize()
 
-    assert serialization_to_tensor(serialization).shape == torch.Size([901])
+    assert serialization_to_tensor(serialization).shape == (20, 8, 8)
+
+
+def test_process_move_coords():
+    # queen move right 1 square
+    move = chess.Move(from_square=4, to_square=5)
+    assert process_move_coords(move) == (4, 0, 6)
+
+    # queen move left 1 square
+    move = chess.Move(from_square=4, to_square=3)
+    assert process_move_coords(move) == (4, 0, 0)
+
+    # queen move left 2 square
+    move = chess.Move(from_square=4, to_square=2)
+    assert process_move_coords(move) == (4, 0, 8)
+
+    # knight move
+    move = chess.Move(from_square=4, to_square=21)
+    assert process_move_coords(move) == (4, 0, 56)
+
+    # knight reversed
+    move = chess.Move(from_square=21, to_square=4)
+    assert process_move_coords(move) == (5, 2, 61)
+
+    # normal pawn promotion
+    move = chess.Move(from_square=53, to_square=61, promotion=chess.QUEEN)
+    assert process_move_coords(move) == (5, 6, 4)
+
+    # pawn forward promotion to knight
+    move = chess.Move(from_square=53, to_square=61, promotion=chess.KNIGHT)
+    assert process_move_coords(move) == (5, 6, 64)
+
+    # pawn diagonal promotion to bishop
+    move = chess.Move(from_square=53, to_square=62, promotion=chess.BISHOP)
+    assert process_move_coords(move) == (5, 6, 71)
